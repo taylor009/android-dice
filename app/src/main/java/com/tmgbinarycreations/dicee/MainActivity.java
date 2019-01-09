d91e963c7c8effa9e0a5d00dbd1dca7c2deeb551
taylor009 <taylor@baremetalcloud.com>
2019-01-08 21:44:38 -0500

    Created button event listener for app

--- app/src/main/java/com/tmgbinarycreations/dicee/MainActivity.java
+++ app/src/main/java/com/tmgbinarycreations/dicee/MainActivity.java
@@ -2,6 +2,10 @@ package com.tmgbinarycreations.dicee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollButton;
+
+        rollButton = (Button) findViewById(R.id.rollButton);
+
+        ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
+
+        ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Dicee", "Ze Button has been pressed!");
+            }
+        });
     }
 }
